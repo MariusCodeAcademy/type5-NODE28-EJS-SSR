b1528801edb086c4c295cdfe2c2419a63ec586cb
Marius <marius@example.com>
2022-03-15 15:59:25 +0200

    active nav, users db

--- src/server.js
+++ src/server.js
@@ -15,8 +15,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'src/views');
 
 // static directory for css, img, js front files
-const staticPath = path.join(__dirname, 'assets')
-app.use(express.static(staticPath))
+const staticPath = path.join(__dirname, 'assets');
+app.use(express.static(staticPath));
 
 // middleware
 app.use(morgan('dev'));
@@ -27,7 +27,8 @@ app.get('/', (req, res) => {
   const data = {
     title: 'Homepage 1',
     age: 25,
-    jsFileName: 'main.js'
+    jsFileName: 'main.js',
+    page: 'home',
   };
   res.render('index', data);
 });
@@ -36,12 +37,14 @@ app.get('/about', (req, res) => {
   const data = {
     title: 'About us',
     techArr: ['HTML', 'CSS', 'JS', 'Node'],
+    page: 'about',
   };
   res.render('about', data);
 });
 app.get('/contact', (req, res) => {
   const data = {
     title: 'Contact us',
+    page: 'contact',
   };
   res.render('contact', data);
 });
@@ -50,8 +53,9 @@ app.all('*', (req, res) => {
   // 404
   const data = {
     title: '404 not found',
+    page: '404',
   };
   res.render('404', data);
-})
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
